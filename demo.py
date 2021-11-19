1bba6ec79c64e02ec5c2f6c47711efdc23bf1211
Sukhleen Kaur <sukhleen999@gmail.com>
2021-11-19 13:46:17 -0800

    added optional argument (ex8)

---
+++ demo.py
@@ -0,0 +1,18 @@
+# author: Sukhleen Kaur
+# date: 2021-11-19
+
+"""This script prints out docopt args.
+Usage: demo.py <arg1> --arg2=<arg2> [--arg3=<arg3>] [<arg4>]
+
+Options:
+<arg>             Takes any value (this is a required positional argument)
+--arg2=<arg2>     Takes any value (this is a required option)
+[--arg3=<arg3>]   Takes any value (this is an optional option)
+[<arg4>]             Takes any value (this is an optional positional argument)
+""" 
+
+from docopt import docopt
+opt = docopt(__doc__)
+print(opt)
+print(type(opt))
+print(opt['<arg4>'])
